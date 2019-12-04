56734b9c0f899b28cc490ca7a1051c1336643a41
Eric Jones <erjones@tael.usersys.redhat.com>
2019-12-04 10:26:58 -0500

    answering part1 for day4 as well as an attempt at better code. its cleaner and only loops once, but the extra check appears to slow it down a good bit

---
+++ day4/part1.slowerbutcleaner.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+import sys
+from os import path
+from math import floor
+
+
+def main(lower, upper):
+    # This will track the number of passwords that meet the criteria
+    answer = 0
+    # We will use this to shrink our options down from lower-upper down to a list of numbers that only increase (or remain the same) from left to right
+    increasing = []
+    # Start by looping over All of the numbers in out range!
+    for number in range(lower, upper+1):
+        # Set this to 0 so we track the position in the 6 digit number 
+        place = 0
+        # Set double to False so that each number is assumed (until proven otherwise) to NOT have a double in it
+        double = False
+        # Set the array to empty so it starts that way for each number
+        increase = []
+        # Loop over each digit in the number
+        for part in str(number):
+            # So long as we are not looking at the first digit
+            if place > 0:
+                # check if the current digit is greater than or equal to the digit immediately before it
+                if int(part) >= int(str(number)[place-1]):
+                    # If it is then add True to the list
+                    increase.append(True)
+                    # Also check if it is specifically the SAME as the digit before
+                    if int(part) == int(str(number)[place - 1]):
+                        # And flip our flag so we now it met this criteria too
+                        double = True
+                # otherwise add False to the list
+                else:
+                    increase.append(False)
+            # Move the location of the counter forward to make it easier to track the spot
+            place += 1
+        # If ALL of the digits are equal to or greater than the ones before it, then add it to our new list
+        if False not in increase:
+            if double:
+                answer += 1
+    print("In the range of {0}-{1}, there are {2} possible passwords that meet the criteria".format(lower, upper, answer))
+
+
+if __name__ == "__main__":
+    inpt = "265275-781584"
+    lower = int(inpt.split("-")[0])
+    upper = int(inpt.split("-")[1])
+    main(lower, upper)
